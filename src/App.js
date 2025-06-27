878bb948a0204f608e701557bbc87984c50e7410
alikhantalal <alikhantalal7@gmail.com>
2025-06-27 12:55:50 +0500

    Added Google site verification meta tag

--- src/App.js
+++ src/App.js
@@ -1,11 +1,20 @@
 import React from 'react';
 import Home from './pages/Home';
+import { Helmet } from 'react-helmet';
 
 function App() {
   return (
-    <div style={{ backgroundColor: '#f3f4f6', minHeight: '100vh' }}>
-      <Home />
-    </div>
+    <>
+      <Helmet>
+        <meta
+          name="google-site-verification"
+          content="JK2Jh-ua8QBYd0zlapWe0zCBUaTzHBMEEoR7wncwEyg"
+        />
+      </Helmet>
+      <div style={{ backgroundColor: '#f3f4f6', minHeight: '100vh' }}>
+        <Home />
+      </div>
+    </>
   );
 }
 
